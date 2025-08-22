0f6cdb3a9a4b3c272e07a7390d3704e175c27e8d
abraaocrvlh42 <abraaoncrvlh@gmail.com>
2025-08-21 23:18:21 -0300

    Starting project

---
+++ src/test/java/com/oak_software/user_registration/UserRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oak_software.user_registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
